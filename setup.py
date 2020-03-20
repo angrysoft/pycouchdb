24b01075e57d309ec067a4d09198cc90e9564663
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-03-21 00:17:34 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup
-from sphinx.setup_command import BuildDoc
-cmdclass = {'build_sphinx': BuildDoc}
 
 name = 'pycouchdb'
 version = '0.1'
@@ -13,11 +11,4 @@ setup(
     license='Apache 2.0',
     author='AngrySoft',
     author_email='sebastian.zwierzchowski@gmail.com',
-    description='Python module for couchdb',
-    cmdclass=cmdclass,
-    command_options={
-        'build_sphinx': {
-            'project': ('setup.py', name),
-            'version': ('setup.py', version),
-            'source_dir': ('setup.py', 'docs')}}
-)
+    description='Python module for couchdb')
