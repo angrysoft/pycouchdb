5bce71a73c8f43217df8cfa3a3e917057bf67828
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-03-21 08:23:58 +0100

    sob, 21 mar 2020, 08:23:58 CET

--- setup.py
+++ setup.py
@@ -11,10 +11,6 @@ setup(
     license='Apache 2.0',
     author='AngrySoft',
     author_email='sebastian.zwierzchowski@gmail.com',
-<<<<<<< HEAD
-    description='Python module for couchdb')
-=======
     description='Python module for couchdb',
     scripts=['tools/dumpdb.py', 'tools/restoredb.py'],
 )
->>>>>>> 585a0a8017c9a2f80cb6ecc95ca534124adb6e5b
