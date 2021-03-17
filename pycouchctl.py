609b9832f3d9d8abef6645f2c1b5bd1426df68dc
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-03-17 12:08:20 +0100

    śro, 17 mar 2021, 12:08:20 CET

--- pycouchctl.py
+++ pycouchctl.py
@@ -5,72 +5,44 @@ import argparse
 from typing import Dict, Any
 from pycouchdb.client import Client
 
-class Opts:
-    pass
 
-class RestoreAction(argparse.Action):
-    def __init__(self, option_strings, dest, nargs=None, **kwargs):
-        if nargs is not None:
-            raise ValueError("nargs not allowed")
-        super().__init__(option_strings, dest, **kwargs)
-        
-    def __call__(self, parser, namespace, values, option_string=None):
-        print('%r %r %r' % (namespace, values, option_string))
-        setattr(namespace, self.dest, values)
-    
-
-class DumpAction(argparse.Action):
-    def __init__(self, option_strings, dest, nargs=None, **kwargs):
-        print(option_strings, dest, kwargs)
-        if nargs is not None:
-            raise ValueError("nargs not allowed")
-        super().__init__(option_strings, dest, **kwargs)
-        
-    def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, values)
-        print(f'Dump database: {values} to file {values}.json')
-        print(namespace.url)
-        # cli = Client(url)
-        # _data: Dict[str, Any] = {db_name: []}
-        # db = cli.get_db(db_name)
-        # for doc in db:
-        #     del doc['_rev']
-        #     _data[db_name].append(doc)
-        
-        # with open(f'{db_name}.json', 'w') as jfile:
-        #     json.dump(_data, jfile)
-
-def load(db_name:str, url:str):
-    cli = Client(url)
+def load(db_name:str, client: Client):
     with open(f'{db_name}.json', 'r') as jfile:
         db_data = json.load(jfile)
-        if db_name in cli:
-            cli.delete(db_name)
-        cli.create(db_name)
-        db = cli.get_db(db_name)
+        if db_name in client:
+            client.delete(db_name)
+        client.create(db_name)
+        db = client.get_db(db_name)
         print(f'Restoring {db_name}')
         for doc in db_data[db_name]:
             db.add(doc)
 
-def dump(db_name:str, url:str):
+
+def dump(db_name:str, client: Client):
     print(f'Dump database: {db_name} to file {db_name}.json')
-    cli = Client(url)
     _data: Dict[str, Any] = {db_name: []}
-    db = cli.get_db(db_name)
-    for doc in db:
+    db = client.get_db(db_name)
+    for doc_id in db:
+        doc = db[doc_id]
         del doc['_rev']
         _data[db_name].append(doc)
-        
+    
     with open(f'{db_name}.json', 'w') as jfile:
-        json.dump(_data, jfile)
+        json.dump(_data, jfile, indent=4)
+
 
 if __name__ == "__main__":
-    opt = Opts()
     parser = argparse.ArgumentParser(usage='%(prog)s [options] [packages]')
-    # parser.add_argument('dbs', nargs='*')
+    parser.add_argument('dbs', nargs='*')
     parser.add_argument('-u', '--url', help='url sever')
-    parser.add_argument('-d', '--dump', action=DumpAction, help="Dump database to json file")
-    parser.add_argument('-r', '--restore', action=RestoreAction, help="Restore database from json file")
+    parser.add_argument('-d', '--dump', action="store_true", help="Dump database to json file")
+    parser.add_argument('-r', '--restore', action="store_true", help="Restore database from json file")
     
     args = parser.parse_args()
-    print(args)
+    client = Client(args.url)
+    if args.dump:
+        for db_name in args.dbs:
+            dump(db_name, client)
+    elif args.restore:
+        for db_name in args.dbs:
+            load(db_name, client)
